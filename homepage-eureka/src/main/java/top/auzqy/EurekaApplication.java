124be80b92b7060977e851bd4e949cbed8fb0c39
au <auzhouqingyu@163.com>
2019-12-16 19:19:42 +0800

    single homepage-eureka finished

--- homepage-eureka/src/main/java/top/auzqy/EurekaApplication.java
+++ homepage-eureka/src/main/java/top/auzqy/EurekaApplication.java
@@ -1,9 +1,23 @@
 package top.auzqy;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * description:  eureka 的入口启动类
+ *
+ * 1. 只需要使用 @EnableEurekaServer 注解就可以让应用变为 Eureka Server
+ * 2. pom 文件中对应到 spring-cloud-starter-netflix-eureka-server
+ *
  * createTime: 2019-12-16 18:25
  * @author au
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 }
