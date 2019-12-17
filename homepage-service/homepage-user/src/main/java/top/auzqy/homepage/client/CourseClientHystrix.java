ec4756d46134bd23d1f0afd7f87a2b19366f84ea
au <auzhouqingyu@163.com>
2019-12-17 21:41:48 +0800

    add dao and client for homepage-user

--- homepage-service/homepage-user/src/main/java/top/auzqy/homepage/client/CourseClientHystrix.java
+++ homepage-service/homepage-user/src/main/java/top/auzqy/homepage/client/CourseClientHystrix.java
@@ -9,8 +9,13 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * <h1>熔断降级策略</h1>
- * Created by Qinyi.
+ * description:  <h1>熔断降级策略</h1>
+ *
+ * 我们要是用熔断降级，这个方法名必须要一样，
+ * 所以为了方便我们直接实现这个接口就可以了。
+ *
+ * createTime: 2019-12-17 21:40
+ * @author au
  */
 @Component
 public class CourseClientHystrix implements CourseClient {
